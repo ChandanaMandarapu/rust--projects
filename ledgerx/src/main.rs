af0933d0a482f6e8e45a2267cbdd591f7db1403f
ChandanaMandarapu <chandanamandarapu93@gmail.com>
2026-01-01 20:20:25 +0530

    initial commits

--- ledgerx/src/main.rs
+++ ledgerx/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+struct Account {
+    id: String,
+    balance: i64,
+} 
+fn main(){
+    let mut ram = Account {
+        id: String::from("ram"),
+        balance:0,
+    };
+
+    ram.balance += 100;
+    println!("Balance {}",ram.balance);
+
 }
